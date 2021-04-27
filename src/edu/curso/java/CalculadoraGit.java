85ef098b1fa7fdff8c9f93dbd759781dc17fe235
lu182 <laf.0182@gmail.com>
2021-04-27 03:37:53 -0300

    Se crea la clase CalculadoraGit y el main

--- src/edu/curso/java/CalculadoraGit.java
+++ src/edu/curso/java/CalculadoraGit.java
@@ -3,5 +3,22 @@ package edu.curso.java;
 public class CalculadoraGit {
 
 	
+	public Double sumar(Double num1, Double num2) {
+		return num1 + num2;
+	}
+	
+	public Double restar(Double num1, Double num2) {
+		return num1 - num2;
+	}
+	
+	
+	
+	public Double multiplicar(Double num1, Double num2) {
+		return num1 * num2;
+	}
+	
+	public Double dividir(Double num1, Double num2) {
+		return num1 / num2;
+	}
 	
 }
--- src/edu/curso/java/PrincipalGit.java
+++ src/edu/curso/java/PrincipalGit.java
@@ -10,6 +10,43 @@ public class PrincipalGit {
 		
 		CalculadoraGit calculadora = new CalculadoraGit();
 		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Ingrese primer número: ");
+		
+		try {
+			System.out.println("Inicio del Try");
+			
+			Double num1 = teclado.nextDouble();
+			
+			System.out.println("Ingrese segundo número: ");
+			Double num2 = teclado.nextDouble();
+			
+			Double totalSuma = 0.0;		
+			totalSuma = calculadora.sumar(num1, num2);
+			System.out.println("La suma es: " + totalSuma);
+			
+			Double totalResta = 0.0;		
+			totalResta = calculadora.restar(num1, num2);
+			System.out.println("La resta es: " + totalResta);
+			
+			Double totalMultiplicacion = 0.0;		
+			totalMultiplicacion = calculadora.multiplicar(num1, num2);
+			System.out.println("La multiplicación es: " + totalMultiplicacion);
+			
+			Double totalDivision = 0.0;		
+			totalDivision = calculadora.dividir(num1, num2);
+			System.out.println("La división es: " + totalDivision);
+			
+			System.out.println("Fin del Try");
+			
+		} catch (Exception e) {
+			System.out.println("Hay un error general...." + e.getMessage());
+			e.printStackTrace();
+			
+		}finally {
+			System.out.println("Listo terminamos");
+		}
+		
 		
 	}
 
